032277ce5424bf3bd794e76a6ceaa865ca2faecf
Medarametla Alekhya <medarametla.alekhya@hcl.com>
2023-08-01 12:39:23 +0530

    ep-3 exploring the JSX concept

--- App.js
+++ App.js
@@ -1,26 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-{
-  /* <div>
-    <h1></h1>
-    <h2></h2>
-    <h3></h3>
-    <div>
-    <h1/></div>
-</div> */
-}
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [
-    React.createElement("h1", { id: "h1tag" }, "i am h1 tag here"),
-    React.createElement("h2", { id: "h2tag" }, "i am h2 tag here"),
-    React.createElement("h3", { id: "h3tag" }, "i am h3 tag here"),
-  ],
-  React.createElement("div", { id: "childDiv" }, [
-    React.createElement("h1", { id: "h1tag" }, "i am h1 tag here"),
-  ])
+// JSX (transpiled before it reaches the Js) - parcel - babel.
+
+// JSX ==> Babel transpiles it to React.createElement ==> ReactElement - JS Object => HtmlElement(render)
+
+const jsxheading= (
+  <h1 className="heading">react using JSX</h1>
 );
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxheading)
